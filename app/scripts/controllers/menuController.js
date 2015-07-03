77c2e343ed704002fa36a2cce257a0eba25357ea
ravi <ravi.m@techjini.com>
2015-07-03 10:32:36 +0530

    Initial commit

--- app/scripts/controllers/menuController.js
+++ app/scripts/controllers/menuController.js
@@ -15,7 +15,7 @@ angAuth.controller('menuController', function ($scope, $location) {
          //You may be thinking 'getLoginStatus' checking  may not be necessary, but is very much required,
         //because if the facebook is logout then calling the FB.logout is an error, and will throw error
         //So better check the status and logout the user.
-
+            console.log('clicked on logout');
             FB.getLoginStatus(function(response) {
                 if (response && response.status === 'connected') {
                     FB.logout(function(response) {
